aaee6081d1a413c967005e91002a47079124ce49
서해윤 <127701730+haennni@users.noreply.github.com>
2025-01-26 18:23:37 +0900

    Title: 이상한 진수 2, Time: 110ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250126/이상한 진수 2/awkward-digits-2.java
+++ 250126/이상한 진수 2/awkward-digits-2.java
@@ -3,33 +3,38 @@ import java.lang.*;
 
 public class Main {
     public static int S(int[] num){
-        int sum = 0;
-        for (int i = 0; i < num.length; i++){
-            if (num[i] == 0) {
-                num[i] = 1;
-                break;
+            int sum = 0;
+            for (int i = 0; i < num.length; i++){
+                if (num[i] == 1){
+                    sum += (int) Math.pow(2, (num.length -1) - i);
+                }
             }
+            return sum;
         }
 
-        for (int i = 0; i < num.length; i++){
-            if (num[i] == 1){
-                if (i == num.length - 1) sum += 1;
-                sum += (int) Math.pow(2, (num.length -1) - i);
+        public static int maxValue(int[] num) {
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < num.length; i++){
+                if (num[i] == 0) {
+                    num[i] = 1 - num[i];
+                    int result = S(num);
+                    max = Math.max(max, result);
+                    num[i] = 1 - num[i];
+                }
             }
+            return max;
         }
-        return sum;
-    }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String line = sc.nextLine();
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String line = sc.nextLine();
 
-        char[] c = line.toCharArray();
-        int[] arr = new int[line.length()];
+            char[] c = line.toCharArray();
+            int[] arr = new int[line.length()];
 
-        for (int i = 0; i < c.length; i++){
-            arr[i] = Character.getNumericValue(c[i]);
+            for (int i = 0; i < c.length; i++){
+                arr[i] = Character.getNumericValue(c[i]);
+            }
+            System.out.println(maxValue(arr));
         }
-        System.out.println(S(arr));
     }
-}
