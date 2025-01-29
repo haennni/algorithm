92543d656c405390ffa1f2c1cdc80e24d211fdba
서해윤 <127701730+haennni@users.noreply.github.com>
2025-01-29 18:35:59 +0900

    Title: G or H 3, Time: 137ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250129/G or H 3/G-or-H-3.java
+++ 250129/G or H 3/G-or-H-3.java
@@ -2,30 +2,41 @@ import java.util.*;
 import java.lang.*;
 
 public class Main {
-    public static int cal(int K, String[][] arr){
-        int sum = 0;
-        for (int i = 0; i < arr.length; i++){
-            if(Integer.parseInt(arr[i][0]) <= K + 1){
-                sum += arr[i][1].equals("G") ? 1 : 2;
+    public static final int MAX_NUM = 10000; 
+    public static int n, k;
+    public static int[] arr = new int[MAX_NUM + 1];
+
+    public static int cal(){
+        int maxSum = 0;
+        for (int i = 0; i <= MAX_NUM - k; i++){
+            int sum = 0;
+            for (int j = i; j < i + k; j++){
+                sum += arr[j];
             }
+            maxSum = Math.max(maxSum, sum);
         }
 
+        return maxSum;
 
-        return sum;
     }
 
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt(); //N명의 사람
-        int K = sc.nextInt(); //사진의 크기 'K' or 'G'
-        String[][] arr = new String[N][2];
+        n = sc.nextInt();
+        k = sc.nextInt(); 
 
-        for(int i = 0; i < N; i++){
-            arr[i][0] = String.valueOf(sc.nextInt());
-            arr[i][1] = sc.next();
+        for(int i = 0; i < n; i++){
+            int x = sc.nextInt();
+            char c = sc.next().charAt(0);
+
+            if (c == 'H'){
+                arr[x] = 1;
+            }else{
+                arr[x] = 2;
+            }
         }
-
-        System.out.println(cal(K, arr));
+        System.out.println(cal());
     }
     
 }
