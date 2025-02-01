f49a8a6d8185e71a3f27f0ba3c2510345435db22
서해윤 <127701730+haennni@users.noreply.github.com>
2025-02-01 21:08:55 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 177ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 250201/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+class Position{
+    int x;
+    int y;
+
+    Position(int y, int x){
+        this.y = y;
+        this.x = x;
+    }
+}
+
+class Main {
+    static int[][] map;
+    static int[] dx;
+    static int[] dy;
+    static boolean[][] visited;
+
+    public static int run(int[][] map){
+        dx = new int[]{0, 1, 0, -1};
+        dy = new int[]{-1, 0, 1, 0};
+
+        Queue<Position> queue = new LinkedList<>();
+        queue.offer(new Position(0, 0));
+
+        while(!queue.isEmpty()){
+            Position position = queue.poll();
+
+            if (position.y == map.length - 1 && position.x == map[0].length - 1) return 1;
+
+            if (visited[position.y][position.x]) continue;
+            visited[position.y][position.x] = true;
+
+            for (int i = 0; i < 4; i++){
+                if (canGo(position, i)) queue.offer(new Position(position.y + dy[i],position.x + dx[i]));
+            }
+        }
+        return 0;
+    }
+
+    public static boolean canGo(Position position, int num){
+        if (position.y + dy[num] < map.length && position.x + dx[num] < map[0].length &&
+            position.x + dx[num] >= 0 && position.y + dy[num] >= 0){
+            return map[position.y + dy[num]][position.x + dx[num]] == 1;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        map = new int[y][x];
+        visited = new boolean[y][x];
+        for (int i = 0; i < y; i++){
+            for (int j = 0; j < x; j++){
+                map[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println(run(map));
+    }
+}
