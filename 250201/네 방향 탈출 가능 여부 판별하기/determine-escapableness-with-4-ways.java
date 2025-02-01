c96ccea4302142a1de92ea6f79ee2bb464f80c1d
서해윤 <127701730+haennni@users.noreply.github.com>
2025-02-01 21:48:11 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 174ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 250201/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
+++ 250201/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
@@ -16,43 +16,40 @@ class Main {
     static int[] dy;
     static boolean[][] visited;
 
-    public static int run(int[][] map){
+    public static int run(int[][] map) {
         dx = new int[]{0, 1, 0, -1};
         dy = new int[]{-1, 0, 1, 0};
+        if (map[0][0] == 0 || map[map.length - 1][map[0].length - 1] == 0) return 0;
 
         Queue<Position> queue = new LinkedList<>();
         queue.offer(new Position(0, 0));
         visited[0][0] = true;
 
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             Position position = queue.poll();
 
             if (position.y == map.length - 1 && position.x == map[0].length - 1) return 1;
 
-            for (int i = 0; i < 4; i++){
-                if (canGo(position, i)) queue.offer(new Position(position.y + dy[i],position.x + dx[i]));
+            for (int i = 0; i < 4; i++) {
+                int ny = position.y + dy[i];
+                int nx = position.x + dx[i];
+                if (canGo(ny, nx)) {
+                    visited[ny][nx] = true;
+                    queue.offer(new Position(ny, nx));
+                }
+
             }
         }
         return 0;
     }
 
-    public static boolean canGo(Position position, int num){
-        if (position.y + dy[num] < map.length && position.x + dx[num] < map[0].length &&
-            position.x + dx[num] >= 0 && position.y + dy[num] >= 0){
-            if (isVisited(position.y + dy[num], position.x + dx[num])){
-                return map[position.y + dy[num]][position.x + dx[num]] == 1;
-            }
-        }
-        return false;
-    }
 
-    public static boolean isVisited(int y, int x){
-        if (visited[y][x]){
-            return false;
-        }else{
-            visited[y][x] = true;
-            return true;
-        }
+    public static boolean canGo(int ny, int nx){
+        /*범위 검사*/
+        if (ny >= map.length || nx >= map[0].length || ny < 0 || nx < 0) return false;
+        /*방문 검사*/
+        if (visited[ny][nx])  return false;
+        return map[ny][nx] == 1;
     }
 
     public static void main(String[] args) {
