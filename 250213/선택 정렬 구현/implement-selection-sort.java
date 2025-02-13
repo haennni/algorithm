b359a2412439f5e55f4d793280e59566e36a4e5d
서해윤 <ggomulll3@gmail.com>
2025-02-13 22:07:46 +0900

    Title: 선택 정렬 구현, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

--- 250213/선택 정렬 구현/implement-selection-sort.java
+++ 250213/선택 정렬 구현/implement-selection-sort.java
@@ -1,19 +1,17 @@
 import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int size = sc.nextInt();
         int[] arr = new int[size];
-
-        for (int i = 0; i < arr.length; i++){
+        for (int i = 0; i < size; i++){
             arr[i] = sc.nextInt();
         }
 
-        for (int i = 0; i < arr.length; i++){
+        for (int i = 0; i < size; i++){
             int min = i;
-            for (int j = i + 1; j < arr.length; j++){
+            for (int j = i + 1; j < size; j++){
                 if (arr[j] < arr[min]){
                     min = j;
                 }
@@ -22,9 +20,8 @@ public class Main {
             arr[i] = arr[min];
             arr[min] = tmp;
         }
-
-        for (int i = 0; i < arr.length; i++){
-            System.out.print(arr[i] + " ");
+        for (int a : arr){
+            System.out.print(a + " ");
         }
     }
 }
