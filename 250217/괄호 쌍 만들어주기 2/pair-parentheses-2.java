e8fdad58e383970b319005a9a9cbaa1c2711f5a3
서해윤 <ggomulll3@gmail.com>
2025-02-17 18:51:55 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 111ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250217/괄호 쌍 만들어주기 2/pair-parentheses-2.java
+++ 250217/괄호 쌍 만들어주기 2/pair-parentheses-2.java
@@ -4,30 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
-        int count = 1;
-        int left_max = Integer.MIN_VALUE;
-        int rigth_max = Integer.MIN_VALUE;
-        /*(의 최댓값을 찾자*/
+        int count = 0;
+    
         for (int i = 0; i < str.length() - 1; i++){
-            if (str.charAt(i) == '(') {
-                if (str.charAt(i + 1) == '(')
-                count++;
-            }else{
-                left_max = Math.max(left_max, count);
+            if (str.charAt(i) == '(' && str.charAt(i + 1) == '(') {
+                for (int j = 0; j < str.length() - 1; j++){
+                    if (str.charAt(j) == ')' && str.charAt(j + 1) == ')')
+                    count++;
+                }
             }
         }
-        count = 1;
-        for (int i = 0; i < str.length() - 1; i++){
-        if (str.charAt(i) == ')') {
-            if (str.charAt(i + 1) == ')')
-            count++;
-        }else{
-            rigth_max = Math.max(rigth_max, count);
-            }
-        }
-        if (rigth_max <= left_max)
-            System.out.println(rigth_max * 2);
-        else
-            System.out.println(left_max * 2);
+        System.out.println(count);
     }
 }
