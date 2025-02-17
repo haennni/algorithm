eecc457c8f4c7864a5aacb248a0d1e00ecaec571
서해윤 <ggomulll3@gmail.com>
2025-02-17 18:56:39 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

--- 250217/괄호 쌍 만들어주기 2/pair-parentheses-2.java
+++ 250217/괄호 쌍 만들어주기 2/pair-parentheses-2.java
@@ -7,13 +7,12 @@ public class Main {
         int count = 0;
     
         for (int i = 0; i < str.length() - 1; i++){
-            if (str.charAt(i) == '(' && str.charAt(i + 1) == '(') {
-                for (int j = 0; j < str.length() - 1; j++){
-                    if (str.charAt(j) == ')' && str.charAt(j + 1) == ')')
+            for (int j = i + 1; j < str.length() - 1; j++){
+                    if (str.charAt(i) == '(' && str.charAt(i + 1) == '('
+                    && str.charAt(j) == ')' && str.charAt(j + 1) == ')')
                     count++;
                 }
             }
-        }
         System.out.println(count);
     }
 }
