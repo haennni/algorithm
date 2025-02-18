b2468e06c887dcac725ca9a3a5b5b2fb44208f77
서해윤 <ggomulll3@gmail.com>
2025-02-18 18:25:39 +0900

    [level 2] Title: 주식가격, Time: 21.65 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,18 +1,19 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] prices) {
-        int[] result = new int[prices.length];
-
-        for (int i = 0; i < prices.length; i++) {
-            int count = 0;
-            for (int j = i + 1; j < prices.length; j++) {
-                count++;
+        int[] arr = new int[prices.length];
+        Arrays.fill(arr, 1);
+        for (int i = 0; i < prices.length - 1; i++){
+            for (int j = i + 1; j < prices.length - 1; j++){
                 if (prices[i] > prices[j]) {
                     break;
+                }else {
+                    arr[i] += 1;
                 }
             }
-            result[i] = count;
         }
-
-        return result;
+        arr[arr.length - 1] = 0;
+        return arr;
     }
 }
