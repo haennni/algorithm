79dc9c465e8461f0dfb43a8593c396aeb5203fa1
서해윤 <ggomulll3@gmail.com>
2025-02-20 16:28:59 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 101.71 ms, Memory: 97.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,19 +1,33 @@
 import java.util.*;
 
 class Solution {
-    public String solution(String[] participant, String[] completion) {
-        Map<String, Integer> map = new HashMap<>();
-        for (String p : participant)
-            map.put(p, map.getOrDefault(p, 0) + 1);
-        for (String c : completion) {
-            Integer integer = map.get(c);
-            if (integer == 1) {
-                map.remove(c);
+    public String solution(
+        String[] participant, 
+        String[] completion) {
+        // participant = 참여자
+        // completion = 완주자
+        StringBuilder sb = new StringBuilder();
+
+        HashMap<String, Integer> user = new HashMap<>();
+
+        for (String p : participant){
+            user.put(p, user.getOrDefault(p, 0) + 1);
+        }
+
+        for (String c : completion){
+            int f = user.get(c);
+            if (f == 1){
+                user.remove(c); 
             }else {
-                map.put(c, map.get(c) - 1);
+                user.put(c, f - 1);
             }
         }
-        Set<String> strings = map.keySet();
-        return strings.iterator().next();
+        
+        Iterator<String> iterator = user.keySet().iterator();
+        
+        while(iterator.hasNext()){
+           return iterator.next();
+        }
+        return "";
     }
 }
